9936398857a3793b40d61f1b56c7486196e5d47f
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-11-20 22:33:22 +0000

    Create homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>PET MATCH</h1>
+        <p>Find your PURRFECT companion!</p>
       </header>
+      <div>
+        <h2>Project in progress...</h2>
+        <p>Project carried out within the scope of the curricular unit Advanced Technologies for Client-Side, at the University of Aveiro.</p>
+      </div>
     </div>
   );
 }

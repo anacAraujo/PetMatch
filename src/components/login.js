060809c5ca081f3998a62c37ed9d46e907719237
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-11-22 12:56:29 +0000

    Remove comments and add TODOs

--- src/components/login.js
+++ src/components/login.js
@@ -5,6 +5,7 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../layout";
 import loginImg from "../assets/img/login-dog.jpg"
+import getDogs from "./getDogs";
 
 export default function Login() {
     const navigate = useNavigate();
@@ -25,40 +26,6 @@ export default function Login() {
             setNotice("You entered a wrong username or password.");
         }
     }
-
-    // return (
-    //     <div className="container">
-    //         <div className="row m-5 no-gutters shadow-lg">
-    //             <div className="col justify-content-center">
-    //                 <form className="col-md-6 bg-white p-5 form-style">
-    //                     {"" !== notice &&
-    //                         <div className="alert alert-warning" role="alert">
-    //                             {notice}
-    //                         </div>
-    //                     }
-    //                     <h1 className="pb-3">Login</h1>
-    //                     <div className="form-floating mb-3">
-    //                         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="name@example.com" value={email} onChange={(e) => setEmail(e.target.value)}></input>
-    //                         <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-    //                     </div>
-    //                     <div className="form-floating mb-3">
-    //                         <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-    //                         <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    //                     </div>
-    //                     <div className="d-grid">
-    //                         <button type="submit" className="btn btn-primary pt-3 pb-3" onClick={(e) => loginWithUsernameAndPassword(e)}>Submit</button>
-    //                     </div>
-    //                     <div className="mt-3 text-center">
-    //                         <span>Need to sign up for an account? <Link to="../signup">Click here.</Link></span>
-    //                     </div>
-    //                 </form>
-    //             </div>
-    //             <div className="col-md-6 d-none d-md-block">
-    //                 <img src={loginImg} alt="dog" className="img-fluid"></img>
-    //             </div>
-    //         </div>
-    //     </div>
-    // );
 
     return (
         <div className="container">
@@ -83,7 +50,8 @@ export default function Login() {
                             </div>
                             <div className="d-flex align-items-center justify-content-between">
                                 <div className="d-flex align-items-center"><input name="" type="checkbox" value="" /> <span className="pl-2 font-weight-bold">Remember Me</span></div>
-                                <div ><a className="color-purple" href="#">Forget Password?</a></div>
+                                {/* TODO: Add password reset functionality */}
+                                <div ><a className="color-purple" href="#" onClick={() => getDogs()}>Forget Password?</a></div>
                             </div>
                             <div className="pb-2">
                                 <button type="submit" className="btn btn-dark w-100 font-weight-bold mt-2" onClick={(e) => loginWithUsernameAndPassword(e)}>Submit</button>
@@ -91,6 +59,7 @@ export default function Login() {
                         </form>
                         <div className="sideline">OR</div>
                         <div>
+                            {/* TODO: Add Facebook or Google login functionality */}
                             <button type="submit" className="button w-100 font-weight-bold mt-2"><i className="fa fa-facebook" aria-hidden="true"></i> Login With Facebook</button>
                         </div>
                         <div className="pt-4 text-center">

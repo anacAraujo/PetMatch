d6b88baedcaeff5084d5ef9874e8f1923faaa4e2
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-11-22 11:44:40 +0000

    Add bootstrap

--- src/components/profile.js
+++ src/components/profile.js
@@ -19,9 +19,9 @@ export default function Profile() {
     }
 
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-4 text-center">
+        <div className="container mt-5">
+            <div className="row justify-content-center mt-5">
+                <div className="col-md-4 text-center mt-5">
                     <p>Welcome <em className="text-decoration-underline">{user.email}</em>. You are logged in!</p>
                     <div className="d-grid gap-2">
                         <button type="submit" className="btn btn-primary pt-3 pb-3" onClick={(e) => logoutUser(e)}>Logout</button>

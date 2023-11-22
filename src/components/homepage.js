55b791a768dffe067047c63a189def678d518ca6
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-11-22 12:57:09 +0000

    Cgange text

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,16 +1,16 @@
 import React from "react";
 
 export default function HomePage() {
-    return (
-        <div className="App">
-          <header className="App-header">
-            <h1>PET MATCH</h1>
-            <p>Find your PURRFECT companion!</p>
-          </header>
-          <div>
-            <h2>Project in progress...</h2>
-            <p>Project carried out within the scope of the curricular unit Advanced Technologies for Client-Side, at the University of Aveiro.</p>
-          </div>
-        </div>
-      );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>PET MATCH</h1>
+        <p>Find your PURRFECT companion!</p>
+      </header>
+      <div>
+        <h2>Our Mission</h2>
+        <p>Our mission is to find the best match for you and your pet!</p>
+      </div>
+    </div>
+  );
 }

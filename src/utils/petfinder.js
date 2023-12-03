e658f2e8f23b68902a24d3c96efeaee446ff3b35
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-12-03 17:19:09 +0000

    Fix fetch petfinder and organize

---
+++ src/utils/petfinder.js
@@ -0,0 +1,56 @@
+async function getToken() {
+  const CLIENTID = 'WK71sClQEcU6kJerMGLmgNPaJigcuIHwOdFdJFj9oupXOG710l';
+  const CLIENTSECRET = '3thR6A98P4nWHgkULqvkfVzWh36TcLTuMSg8PttZ';
+
+  let token = null;
+
+  try {
+    const response = await fetch('https://api.petfinder.com/v2/oauth2/token', {
+      method: 'POST',
+      body: 'grant_type=client_credentials&client_id=' + CLIENTID + '&client_secret=' + CLIENTSECRET,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+    const tokenData = await response.json();
+    console.log(tokenData);
+
+    token = tokenData.access_token;
+  } catch (error) {
+    console.error('Error when trying to fetch token: ', error);
+  }
+
+  return token;
+}
+
+async function getAnimals(type = 'dog', limit = 2, sort = 'random', good_with_children) {
+  try {
+    let token = await getToken();
+    console.log('token: ' + token);
+
+    const url = new URL('https://api.petfinder.com/v2/animals');
+    url.searchParams.append('type', type);
+    url.searchParams.append('limit', limit);
+    url.searchParams.append('sort', sort);
+
+    if (good_with_children) {
+      url.searchParams.append('good_with_children', good_with_children);
+    }
+
+    const response = await fetch(url.toString(), {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: 'Bearer ' + token,
+      },
+    });
+
+    const result = await response.json();
+    return result.animals;
+  } catch (error) {
+    console.error('Error when trying to fetch animals: ', error);
+  }
+  return [];
+}
+
+module.exports = { getAnimals };

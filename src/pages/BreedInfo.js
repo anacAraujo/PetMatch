5aa8aa6536155c74f446c6a2c8b49c45d1ba7b25
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-12-04 21:31:20 +0000

    Create file

---
+++ src/pages/BreedInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+export default function BreedInfo() {
+  return (
+    <Container>
+      <Row>
+        <h1>Breed Info</h1>
+      </Row>
+    </Container>
+  );
+}
---
+++ src/utils/ninjas.js
@@ -0,0 +1,17 @@
+async function getBreeds(type, breed) {
+  try {
+    const response = await fetch(`https://api.api-ninjas.com/v1/${type}?name=${breed}`, {
+      method: 'GET',
+      headers: {
+        'x-api-key': 'M9VfuEB0q8+EPjNn2tPyWw==ZTMC2Ajxxt9WkR3T',
+      },
+    });
+
+    const result = await response.json();
+    return result.animals;
+  } catch (error) {
+    console.error('Error when trying to fetch breeds: ', error);
+  }
+}
+
+module.exports = { getBreeds };

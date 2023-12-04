8e62b4210fd9229b9bf2d2dc1370a21c98154313
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-12-04 21:31:53 +0000

    Add componente Mission

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import ListAnimals from '../components/ListAnimals';
+import ListAnimals from '../components/Animals/ListAnimals';
+import Mission from '../components/Mission';
 
 import '../assets/styles/pages/Home.scss';
 
@@ -12,10 +13,7 @@ export default function Home() {
         <p>Find your PERFECT companion!</p>
       </header>
       <Container>
-        <div>
-          <h2>Our Mission</h2>
-          <p>Our mission is to find the best match for you and your pet!</p>
-        </div>
+        <Mission></Mission>
         <ListAnimals></ListAnimals>
       </Container>
     </div>

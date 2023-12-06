7bc4eb4b720e1a25661690a658ba12408ad07223
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-12-06 11:16:19 +0000

    Add fetch to api dogceo

---
+++ src/utils/dogceo.js
@@ -0,0 +1,14 @@
+async function getAllBreeds() {
+  try {
+    const response = await fetch(`https://dog.ceo/api/breeds/list/all`, {
+      method: 'GET',
+    });
+
+    const result = await response.json();
+    return result.message;
+  } catch (error) {
+    console.error('Error when trying to fetch breeds: ', error);
+  }
+}
+
+module.exports = { getAllBreeds };

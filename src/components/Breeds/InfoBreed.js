c958d82202aa5b71518b5359d0090fef5f2a0204
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-12-06 15:47:16 +0000

    Add text breed

--- src/components/Breeds/InfoBreed.js
+++ src/components/Breeds/InfoBreed.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import ninjas from '../../utils/ninjas';
 import Col from 'react-bootstrap/Col';
+import ListAnimals from '../Animals/ListAnimals';
 
 export default function InfoBreed() {
   const { breed } = useParams();
@@ -25,9 +26,22 @@ export default function InfoBreed() {
     <Col>
       {infoBreed.map((info) => (
         <div>
-          <p>{info.good_with_children}</p>
+          <img src={info.image_link} alt={info.name} style={{ width: '200px', height: 'auto' }} />
+          <p>
+            The {info.name} is a breed that is {info.good_with_children === 5 ? 'very good' : 'not very good'} with
+            children. It has a shedding level of {info.shedding} out of 5, which means it{' '}
+            {info.shedding > 3 ? 'sheds a lot' : 'does not shed much'}. This breed is{' '}
+            {info.barking === 5 ? 'very vocal' : 'not very vocal'}, with a barking level of {info.barking} out of 5. It
+            has an energy level of {info.energy} out of 5, indicating it is{' '}
+            {info.energy > 3 ? 'high energy' : 'low energy'}. The {info.name} is{' '}
+            {info.protectiveness === 5 ? 'very protective' : 'not very protective'}, with a protectiveness level of{' '}
+            {info.protectiveness} out of 5. This breed is {info.trainability === 5 ? 'very easy' : 'very difficult'} to
+            train, with a trainability level of {info.trainability} out of 5.
+          </p>
         </div>
       ))}
+      <h3>Some {breed}s</h3>
+      <ListAnimals breed={breed}></ListAnimals>
     </Col>
   );
 }

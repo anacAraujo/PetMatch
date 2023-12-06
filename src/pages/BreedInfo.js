1382df01c462be20a71eafa5a5d4c2bb616227eb
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-12-06 11:15:14 +0000

    Add components

--- src/pages/BreedInfo.js
+++ src/pages/BreedInfo.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import InfoBreed from '../components/Breeds/InfoBreed';
+import ListBreeds from '../components/Breeds/ListBreeds';
+import { useParams } from 'react-router-dom';
 
 export default function BreedInfo() {
+  const { breed } = useParams();
   return (
     <Container>
       <Row>
-        <h1>Breed Info</h1>
+        <h1>{breed}</h1>
+        <ListBreeds></ListBreeds>
+        <InfoBreed></InfoBreed>
       </Row>
     </Container>
   );

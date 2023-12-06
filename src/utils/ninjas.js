a665f6d71973ffb621a05f087d61386744a47808
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-12-06 11:16:00 +0000

    Refactor api ninjas fetch

--- src/utils/ninjas.js
+++ src/utils/ninjas.js
@@ -1,4 +1,4 @@
-async function getBreeds(type, breed) {
+async function getInfoBreed(type, breed) {
   try {
     const response = await fetch(`https://api.api-ninjas.com/v1/${type}?name=${breed}`, {
       method: 'GET',
@@ -8,10 +8,10 @@ async function getBreeds(type, breed) {
     });
 
     const result = await response.json();
-    return result.animals;
+    return result;
   } catch (error) {
     console.error('Error when trying to fetch breeds: ', error);
   }
 }
 
-module.exports = { getBreeds };
+module.exports = { getInfoBreed };

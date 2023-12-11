6452a9bd86df740c9478910f9cce656c3f8e58f6
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-12-11 17:27:25 +0000

    Ad footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="bg-body-tertiary text-center text-lg-start">
+      <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2021 Pet Match:
+        <a className="text-dark" href="https://mdbootstrap.com/">
+          {' '}
+          PetMatch.com
+        </a>
+      </div>
+    </footer>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 import ListAnimals from '../components/Animals/ListAnimals';
 import Mission from '../components/Mission';
+import Footer from '../components/Footer';
 
 import '../assets/styles/pages/Home.scss';
 
@@ -13,9 +15,16 @@ export default function Home() {
         <p>Find your PERFECT companion!</p>
       </header>
       <Container>
+        <p>What we belive in</p>
+        <h3 className="mb-5">Our mission is to find the best match for you and your pet!</h3>
         <Mission></Mission>
+        <h3 className="mb-5">Some of our pets available</h3>
         <ListAnimals></ListAnimals>
+        <Button variant="primary" className="m-5 px-5 py-3" href="/login">
+          See More
+        </Button>
       </Container>
+      <Footer></Footer>
     </div>
   );
 }

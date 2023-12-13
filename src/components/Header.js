0c9c1724b1923094bfdfe6c611cbef12c10f9866
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-12-13 22:04:21 +0000

    replace context with firebase auth

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,97 +5,86 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
-export const UserContext = React.createContext();
+import { auth } from '../utils/firebase';
 
 export default function Header() {
-  //TODO: Separar para a pasta de contextos
-  const [isLog, setLog] = useState(false);
-  const updateIsLog = (dadosfilho) => {
-    setLog(dadosfilho);
-  };
-
-  if (isLog === true) {
+  if (auth.currentUser) {
     return (
-      <UserContext.Provider value={{ isLogado: isLog, func: updateIsLog }}>
-        <div>
-          <nav className="navbar navbar-expand-lg fixed-top navbar-light bg-light">
-            <div className="container-fluid">
-              <Link className="navbar-brand" to="/">
-                PET MATCH
-              </Link>
-              <div className="collapse navbar-collapse" id="navbarText">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/profile">
-                      profile
-                    </Link>
-                  </li>
-                </ul>
-                <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/profile">
-                      <IoPersonCircleSharp size={30}></IoPersonCircleSharp>
-                    </Link>
-                  </li>
-                </ul>
-              </div>
+      <div>
+        <nav className="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="/">
+              PET MATCH
+            </Link>
+            <div className="collapse navbar-collapse" id="navbarText">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/profile">
+                    profile
+                  </Link>
+                </li>
+              </ul>
+              <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/profile">
+                    <IoPersonCircleSharp size={30}></IoPersonCircleSharp>
+                  </Link>
+                </li>
+              </ul>
             </div>
-          </nav>
-          <Outlet />
-        </div>
-      </UserContext.Provider>
+          </div>
+        </nav>
+        <Outlet />
+      </div>
     );
   } else {
     return (
-      <UserContext.Provider value={{ isLogado: isLog, func: updateIsLog }}>
-        <div>
-          <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-            <Container fluid>
-              <Link className="navbar-brand" to="/">
-                PET MATCH
-              </Link>
-              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-              <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="me-auto">
-                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                      <Link className="nav-link" to="/breedinfo/Affenpinscher">
-                        Breeds
-                      </Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="/about">
-                        About
-                      </Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="/quiz">
-                        Quiz
-                      </Link>
-                    </li>
-                  </ul>
+      <div>
+        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+          <Container fluid>
+            <Link className="navbar-brand" to="/">
+              PET MATCH
+            </Link>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/breedinfo/Affenpinscher">
+                      Breeds
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/about">
+                      About
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/quiz">
+                      Quiz
+                    </Link>
+                  </li>
+                </ul>
 
-                  <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                  </NavDropdown>
-                </Nav>
+                <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
 
-                <Nav>
-                  <Link className="nav-link" to="/login">
-                    Login
-                  </Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-          <Outlet />
-        </div>
-      </UserContext.Provider>
+              <Nav>
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Outlet />
+      </div>
     );
   }
 }

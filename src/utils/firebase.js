b4c4678abffd7b9625ef05c56918bbc97d262254
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-12-13 15:50:36 +0000

    Add login with google

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,11 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyAJULtP4XEWwn2vS6fCTebTWuCrKRAwuto',
   authDomain: 'petmatch-ebd2b.firebaseapp.com',
@@ -19,5 +15,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
-export { auth };
+export { auth, googleProvider };

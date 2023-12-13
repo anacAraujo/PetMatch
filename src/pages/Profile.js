f6d34aaf73ba387c3ebec4b934610ab0be2b3d65
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-12-13 22:06:28 +0000

    Remove UserContext, replace to bootstrap react tabs, add nav and ListAnimals

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,36 +2,74 @@ import { useNavigate } from 'react-router-dom';
 import { auth } from '../utils/firebase';
 import { signOut } from 'firebase/auth';
 import React from 'react';
-import { UserContext } from '../components/Header';
+import { Nav, Container, Row, Col, Button } from 'react-bootstrap';
+import { useState } from 'react';
+import ListAnimals from '../components/Animals/ListAnimals';
+import { useParams } from 'react-router-dom';
 
 export default function Profile() {
   const navigate = useNavigate();
   const user = auth.currentUser;
 
-  const value = React.useContext(UserContext);
+  const { type, age, size, gender, good_with_children, good_with_dogs, good_with_cats } = useParams();
+
+  const [selectedTab, setSelectedTab] = useState('My Matches');
+  const handleTabClick = (tabName) => {
+    setSelectedTab(tabName);
+  };
 
   const logoutUser = async (e) => {
     e.preventDefault();
 
     await signOut(auth);
-    value.func(false);
     navigate('/');
   };
 
   return (
-    <div className="container mt-5">
-      <div className="row justify-content-center mt-5">
-        <div className="col-md-4 text-center mt-5">
-          <p>
-            Welcome <em className="text-decoration-underline">{user.email}</em>. You are logged in!
-          </p>
-          <div className="d-grid gap-2">
-            <button type="submit" className="btn btn-primary pt-3 pb-3" onClick={(e) => logoutUser(e)}>
-              Logout
-            </button>
-          </div>
+    <Container className=" mt-5">
+      <Row className=" justify-content-center mt-5">
+        <div className=" text-center mt-5">
+          <h2>
+            Welcome <em className="text-decoration-underline">{user?.email}</em>.
+          </h2>
         </div>
-      </div>
-    </div>
+        <Nav variant="tabs" className="m-5">
+          <Nav.Item>
+            <Nav.Link onClick={() => handleTabClick('My Information')}>My Information</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link onClick={() => handleTabClick('My Preferences')}>My Preferences</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link onClick={() => handleTabClick('My Matches')}>My Matches</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Row>
+      <Row>
+        {selectedTab === 'My Information' && (
+          <Col>
+            <h3>My Information</h3>
+            <p>Email: {user?.email}</p>
+            <Button className="my-5 px-4" onClick={(e) => logoutUser(e)}>
+              Logout
+            </Button>
+          </Col>
+        )}
+        {selectedTab === 'My Preferences' && <Row>/* My Preferences content */</Row>}
+        {selectedTab === 'My Matches' && (
+          <Row>
+            <ListAnimals
+              type={type}
+              age={age}
+              size={size}
+              gender={gender}
+              good_with_children={good_with_children}
+              good_with_dogs={good_with_dogs}
+              good_with_cats={good_with_cats}
+            ></ListAnimals>
+          </Row>
+        )}
+      </Row>
+    </Container>
   );
 }

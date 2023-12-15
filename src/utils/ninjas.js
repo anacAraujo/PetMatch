f7a27470983bef7a2f4a54f62e388319122a75f4
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-12-15 10:11:46 +0000

    Add plural to parameter type

--- src/utils/ninjas.js
+++ src/utils/ninjas.js
@@ -1,6 +1,7 @@
 async function getInfoBreed(type, breed) {
+  const pluralType = type + 's';
   try {
-    const response = await fetch(`https://api.api-ninjas.com/v1/${type}?name=${breed}`, {
+    const response = await fetch(`https://api.api-ninjas.com/v1/${pluralType}?name=${breed}`, {
       method: 'GET',
       headers: {
         'x-api-key': 'M9VfuEB0q8+EPjNn2tPyWw==ZTMC2Ajxxt9WkR3T',

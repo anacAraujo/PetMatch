c9684a92e41419f7a9a125cbdb9e2b81fe2b7dfc
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-12-15 12:04:07 +0000

    Add section when fetch returns {}

--- src/components/Animals/ListAnimals.js
+++ src/components/Animals/ListAnimals.js
@@ -2,6 +2,10 @@ import CardAnimal from './CardAnimal';
 import * as petfinder from '../../utils/petfinder';
 import { useState, useEffect } from 'react';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import cat from '../../assets/images/sorry-cat.jpg';
+import dog from '../../assets/images/sorry-dog.jpg';
 
 export default function ListAnimals(props) {
   const [animals, setAnimals] = useState([]);
@@ -30,6 +34,27 @@ export default function ListAnimals(props) {
     getAnimals();
   }, [breed]);
 
+  if (!animals) {
+    return <div>Loading...</div>;
+  }
+  if (animals.length === 0) {
+    return (
+      <Row className="mx-5 mb-5">
+        <Col xs={6} md={4}>
+          {type === 'cat' ? <Image fluid rounded src={cat}></Image> : <Image fluid rounded src={dog}></Image>}
+        </Col>
+        <Col className="mt-5">
+          <h2 className="m-5">Sorry!</h2>
+          <p className="mb-5">No animals found.</p>
+          <p>
+            It seems that there are no pets looking for homes that match what you are looking for! Remember, it's a good
+            thing: this means they've already found their family and their forever home.
+          </p>
+          <p>Try changing your preferences or check back later.</p>
+        </Col>
+      </Row>
+    );
+  }
   return (
     <Row>
       {animals.map((animal) => (

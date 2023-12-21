0ad97246703a40ea48bb1dde691bd54555f3f093
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-12-21 13:48:04 +0000

    Add padding to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
 export default function Footer() {
   return (
     <footer className="bg-body-tertiary text-center text-lg-start">
-      <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+      <div className="text-center p-3 py-4" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
         © 2021 Pet Match:
         <a className="text-dark" href="https://mdbootstrap.com/">
           {' '}
           PetMatch.com
         </a>
-        <p>Choose PetMatch – where lasting relationships between people and pets begin.</p>
+        <p className="m-3">Choose PetMatch – where lasting relationships between people and pets begin.</p>
       </div>
     </footer>
   );
